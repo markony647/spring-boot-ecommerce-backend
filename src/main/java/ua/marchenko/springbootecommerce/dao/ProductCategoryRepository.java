8c459491581c1cd0976dcd3fffb9003349153589
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-07-02 16:09:08 +0300

    Add cross-origin requests support

--- src/main/java/ua/marchenko/springbootecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/ua/marchenko/springbootecommerce/dao/ProductCategoryRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import ua.marchenko.springbootecommerce.entity.ProductCategory;
 
-@CrossOrigin
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category") // needed to override default api/productCategories
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/ua/marchenko/springbootecommerce/dao/ProductRepository.java
+++ src/main/java/ua/marchenko/springbootecommerce/dao/ProductRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import ua.marchenko.springbootecommerce.entity.Product;
 
-@CrossOrigin
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
