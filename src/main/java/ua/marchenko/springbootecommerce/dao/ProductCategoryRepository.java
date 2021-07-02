6cfed20db40a77baed4f9c7fd0134e37607397f3
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-07-02 14:47:11 +0300

    Make REST API read only

--- src/main/java/ua/marchenko/springbootecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/ua/marchenko/springbootecommerce/dao/ProductCategoryRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ua.marchenko.springbootecommerce.entity.ProductCategory;
 
-@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category") // needed to override default api/productCategories
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
